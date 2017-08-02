228f2e9758438e2c0c43f0804b8baaf17566b444
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-02 02:25:47 +0000

    exercise4 finished and changed name of store in exercise2 to prevent repeating names

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name:"Surrey", annual_revenue:223000, mens_apparel:false, womens_apparel:true)
+Store.create(name:"Whistler", annual_revenue:1900000, mens_apparel:true, womens_apparel:false)
+Store.create(name:"Yaletown", annual_revenue:430000, mens_apparel:true, womens_apparel:true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store| 
+  puts "Mens only: #{store.name}, #{store.annual_revenue}" 
+end
+
+@womens_stores = Store.where("womens_apparel= ? and  annual_revenue < ?", true, 1000000)
+@womens_stores.each do |store| 
+  puts "Womens only and less than $1M: #{store.name}, #{store.annual_revenue}" 
+end
