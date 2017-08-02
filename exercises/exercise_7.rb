42f2e2ab46d2ec0a0809d8f1c291fc3a7d1bd793
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-02 16:52:49 +0000

    exercise7 complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+begin
+  p "please enter a store name"
+  @store_name = gets.chomp
+
+  @new_store = Store.create(name: @store_name, annual_revenue: 10)
+  @new_store.save!
+rescue Exception => error
+  puts error.message
+end
+
+
+# test
+# @store1.employees.create(first_name: "kkk",last_name:'slks', hourly_rate: 50)
+
+
+
+
